e98be017a61b32093c973912cd340fc54bca8fc9
ol-bones <obones.wc@gmail.com>
2016-09-11 19:19:02 +0100

    User Registration and very prototype user sessions

--- src/newswebserver/Database.java
+++ src/newswebserver/Database.java
@@ -1,49 +1,236 @@
 package newswebserver;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.locks.*;
+import java.time.*;
+
+class Session
+{
+    private final ReentrantLock lock = new ReentrantLock();
+    
+    private static long TotalSessions = 0;
+    private long m_SessionID;
+    private LocalDate m_ExpiryDate;
+    
+    private Session() { }
+    
+    public long getSessionID()
+    {
+        return this.m_SessionID;
+    }
+    
+    public LocalDate getExpiryDate()
+    {
+        return this.m_ExpiryDate;
+    }
+    
+    public String getSessionDataAsJSONString()
+    {
+        return "{" + "\"sessionID\": \"" + String.valueOf(this.getSessionID()) + "\"}";
+    }
+    
+    public synchronized void setSessionID()
+    {
+        lock.lock();
+        try
+        {
+            this.m_SessionID = TotalSessions;
+            TotalSessions++;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+    
+    public void setDate()
+    {
+        try
+        {
+            this.m_ExpiryDate = LocalDate.now();
+            this.m_ExpiryDate = this.m_ExpiryDate.plusDays(2);
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+    
+    public void generateSession()
+    {
+        lock.lock();
+        try
+        {
+            this.m_SessionID = TotalSessions;
+            TotalSessions++;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+        finally
+        {
+            lock.unlock();
+        }
+    }
+    
+    public static Session getNewSession()
+    {
+        try
+        {
+            Session newSession = new Session();
+            newSession.setSessionID();
+            newSession.setDate();
+            
+            return newSession;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "{ \"session\": {" + "\"sessionID\": \"" + String.valueOf(this.m_SessionID) + "\","
+                + "\"expieryDate\": \"" + String.valueOf(this.m_ExpiryDate.toString()) + "\"}}";
+    }
+}
 
 class User
 {
     public String m_Username;
     public String m_Password;
     public ArrayList<String> m_Subscriptions;
+    public Session m_CurrentSession;
+    
+    public User(String user, String pass)
+    {
+        this.m_Username = user;
+        this.m_Password = pass;
+        
+        this.m_CurrentSession = Session.getNewSession();
+    }
 }
+
+class NewsItem
+{
+    public String m_URI;
+    public String m_Type;
+    
+    public LocalDate m_VersionCreatedDate;
+    public LocalTime m_VersionCreatedTime;
+    
+    public String m_HeadlineText;
+    public String m_BylineText;
+    public String m_BodyText;
+    
+    public String m_Person;
+    public String m_Organisation;
+}
+
 
 public class Database
 {
+    private final ReentrantLock lock = new ReentrantLock();
     public static ArrayList<User> m_UserList = new ArrayList<User>();
     
     public Database()
     {
-        User user1 = new User();
-            user1.m_Username = "Owen";
-            user1.m_Password = "Test";
-            user1.m_Subscriptions = new ArrayList<String>();
-            user1.m_Subscriptions.add("Politics");
-            user1.m_Subscriptions.add("Technology");
-            user1.m_Subscriptions.add("Games");     
+        try
+        {
+            lock.lock();
+            User user1 = new User("Owen", "Test");
         
-        m_UserList.add(user1);
+                user1.m_Subscriptions = new ArrayList<String>();
+                user1.m_Subscriptions.add("Politics");
+                user1.m_Subscriptions.add("Technology");
+                user1.m_Subscriptions.add("Games");     
+
+            m_UserList.add(user1);
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+        finally
+        {
+            lock.unlock();
+        }
     }
     
     public User FindUserByName(String input_user)
     {
-        for(User user : this.m_UserList)
+        try
         {
-           if(user.m_Username.equals(input_user))
-           {
-                return user;
-           }
+            lock.lock();
+            for(User user : this.m_UserList)
+            {
+               if(user.m_Username.equals(input_user))
+               {
+                    return user;
+               }
+            }
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+        finally
+        {
+            lock.unlock();
+        }
+        return null;
+    }
+    
+    public User FindBySessionID(String sessionid)
+    {
+        try
+        {
+            lock.lock();
+            for(User user : this.m_UserList)
+            {
+                if(String.valueOf(user.m_CurrentSession.getSessionID()).equals(sessionid))
+                {
+                    return user;
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+        finally
+        {
+            lock.unlock();
         }
         return null;
     }
     
     public boolean AddUser(User new_user)
     {
-        if(FindUserByName(new_user.m_Username) == null)
+        try
         {
-            m_UserList.add(new_user);
-            return true;
+            lock.lock();
+            if(FindUserByName(new_user.m_Username) == null)
+            {
+                m_UserList.add(new_user);
+                return true;
+            }
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+        finally
+        {
+            lock.unlock();
         }
         return false;
     }
--- src/newswebserver/NewsWebServer.java
+++ src/newswebserver/NewsWebServer.java
@@ -8,6 +8,7 @@ package newswebserver;
 import java.io.*;
 import java.net.*;
 import com.sun.net.httpserver.*;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -28,6 +29,7 @@ public class NewsWebServer
             server.createContext("/signincontent", new SignInRequestHandler());
             server.createContext("/RegisterRequest", new RegisterRequestHandler());
             server.createContext("/LoginRequest", new LoginRequestHandler());
+            server.createContext("/CookieLoginRequest", new CookieLoginRequestHandler());
             server.createContext("/userpagecontent", new UserPageHandler());
             server.setExecutor(Executors.newFixedThreadPool(10)); // creates a default executor
             server.start();
@@ -173,11 +175,9 @@ public class NewsWebServer
                 
                 if(db.FindUserByName(username) == null)
                 {
-                    User registerUser = new User();
-                    registerUser.m_Username = username;
-                    registerUser.m_Password = password;
-                    
+                    User registerUser = new User(username, password);
                     db.AddUser(registerUser);
+                    
                     added = "User added";
                     System.out.println(username + " registered");
                     
@@ -231,9 +231,11 @@ public class NewsWebServer
                 User user = db.FindUserByName(username);
                 if(user != null && user.m_Password.equals(password))
                 {
+                    user.m_CurrentSession.generateSession();
                     HTMLView testView = new HTMLView("content/registercontent.html");
                     String response = ("{\"login\": true, \"username\": \"" + user.m_Username 
-                                       + "\", \"password\": \"" + user.m_Password + "\"}"); 
+                                       + "\", \"password\": \"" + user.m_Password + "\","
+                                       + "\"session\": " + user.m_CurrentSession.getSessionDataAsJSONString() + "}"); 
                     t.sendResponseHeaders(200, response.length());
                     OutputStream os = t.getResponseBody();
                     os.write(response.getBytes());
@@ -248,7 +250,75 @@ public class NewsWebServer
                     OutputStream os = t.getResponseBody();
                     os.write(response.getBytes());
                     os.close();   
-                    System.out.println(username + " failed to log in");
+                    System.out.println(username + " failed to log in (Username/password incorrect)");
+                }
+                
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    static class CookieLoginRequestHandler implements HttpHandler 
+    {
+        public void handle(HttpExchange t) throws IOException 
+        {
+            try
+            {
+                InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
+                BufferedReader br = new BufferedReader(isr);
+
+                int b;
+                StringBuilder buf = new StringBuilder(512);
+                while ((b = br.read()) != -1) {
+                    buf.append((char) b);
+                }
+
+                br.close();
+                isr.close();
+                
+                String buffer = buf.toString();
+                
+                String[] splitParams = buffer.split("&");
+                Map<String, String> params = new HashMap<String, String>();
+                for(String param : splitParams)
+                {
+                    String[] keyValue = param.split("=");
+                    params.put(keyValue[0], keyValue[1]);
+                }
+                
+                String sessionid = params.get("cookie");
+                
+                User user = db.FindBySessionID(sessionid);
+                if(user != null)
+                {
+                    System.out.println("User("+user.m_Username+") found by sessionID: " + String.valueOf(user.m_CurrentSession.getSessionID()));
+                    System.out.println(user.m_CurrentSession.toString());
+                }
+                
+                if(user != null && user.m_CurrentSession.getExpiryDate().isAfter(LocalDate.now()))
+                {
+                    HTMLView testView = new HTMLView("content/registercontent.html");
+                    String response = ("{\"login\": true, \"username\": \"" + user.m_Username 
+                                       + "\", \"password\": \"" + user.m_Password + "\","
+                                       + "\"session\": " + user.m_CurrentSession.getSessionDataAsJSONString() + "}"); 
+                    t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                    System.out.println(user.m_Username + " logged in via cookie");
+                }
+                else
+                {
+                    HTMLView testView = new HTMLView("content/registercontent.html");
+                    String response = ("{\"login\": false, \"sessionid\": \"" + sessionid + "\"}"); 
+                    t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();   
+                    System.out.println(sessionid + " failed to log in (Session(" + sessionid + ") expired or not found)");
                 }
                 
             }
